09055dbeba20caf0b529de25cab0731e7dc9dca4
yuvarajaselvan <152358038+yuvarajaselvan@users.noreply.github.com>
2023-11-29 13:48:10 +0530

    Add files via upload

---
+++ SquareRootExample.java
@@ -0,0 +1,14 @@
+package Squareroot;
+public class SquareRootExample {
+
+    public static void main(String[] args) {
+        
+        int number = 8;
+
+     
+        int squareRoot =(int) Math.sqrt(8);
+
+        // Displaying the result
+        System.out.println("The square root of " + number + " is: " + squareRoot);
+    }
+}
