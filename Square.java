73b8f50ea60ece624fb38616e667ace46e5138c2
yuvarajaselvan <152358038+yuvarajaselvan@users.noreply.github.com>
2023-11-29 12:27:43 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,14 @@
+package Squareroot;
+
+public class Square {
+
+	public static void main(String[] args) {
+		double a = 8; 
+		  
+        System.out.println(Math.sqrt(a)); 
+  
+        a = 8; 
+
+	}
+
+}
