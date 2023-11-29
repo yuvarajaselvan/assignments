9591d237c432ad1e391bbe70527573425acbeebf
yuvarajaselvan <152358038+yuvarajaselvan@users.noreply.github.com>
2023-11-29 09:48:37 +0530

    Add files via upload

---
+++ Week1day1if.java
@@ -0,0 +1,13 @@
+package Assignments;
+
+public class Week1day1if {
+
+	public static void main(String[] args) {
+	int x = 45;
+	if (x>0) {
+	System.out.println("the number is positive");
+	} else {
+		System.out.println("the number is negative");
+	}
+	}
+    }
